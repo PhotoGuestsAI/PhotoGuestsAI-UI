13d09640f106a99979c52a79afc45d7af54b02a1
AmitLustiger <amit@mabaya.com>
2025-02-17 23:58:57 +0200

    moved to albums router

--- src/components/EventDetail.jsx
+++ src/components/EventDetail.jsx
@@ -51,7 +51,7 @@ const EventDetail = () => {
         formData.append("album", albumFile);
 
         try {
-            await axios.post(`http://http://50.19.49.233:8000/events/${eventId}/upload-event-album`, formData, {
+            await axios.post(`http://http://50.19.49.233:8000/albums/${eventId}/upload-event-album`, formData, {
                 headers: {"Content-Type": "multipart/form-data", Authorization: `Bearer ${user?.token}`},
             });
             alert("File uploaded successfully!");
